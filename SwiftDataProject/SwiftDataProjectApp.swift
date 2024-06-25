3f158e97d19c96d003b2ae202a11cb2353665caa
Evdokimov Denis <analitik9@gmail.com>
2024-06-25 16:28:57 +0300

    Initial Commit

---
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Denis Evdokimov on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
