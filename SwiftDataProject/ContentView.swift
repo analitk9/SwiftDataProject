088adb7e3696fa1986b880c6d9f0c3a351449635
Evdokimov Denis <analitik9@gmail.com>
2024-06-26 14:30:06 +0300

    init

--- SwiftDataProject/ContentView.swift
+++ SwiftDataProject/ContentView.swift
@@ -6,16 +6,78 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    @Environment(\.modelContext) var modelContext
+   
+//    @Query(filter: #Predicate<User> { user in
+//        if user.name.localizedStandardContains("R") {
+//            if user.city == "London" {
+//                return true
+//            } else {
+//                return false
+//            }
+//        } else {
+//            return false
+//        }
+//    }, sort: \User.name) var users: [User]
+   
+    @State private var sortOrder = [
+        SortDescriptor(\User.name),
+        SortDescriptor(\User.joinDate),
+    ]
+    
+    @State private var path = [User]()
+    @State private var showingUpcomingOnly = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack(path: $path) {
+            UsersView(minimumJoinDate: showingUpcomingOnly ? .now : .distantPast, sortOrder: sortOrder)
+            .navigationTitle("Users")
+            .navigationDestination(for: User.self) { user in
+                EditUserView(user: user)
+            }
+            .toolbar{
+                Menu("Sort", systemImage: "arrow.up.arrow.down")  {
+                    Picker("Sort", selection: $sortOrder) {
+                        Text("Sort by Name")
+                            .tag([
+                                SortDescriptor(\User.name),
+                                SortDescriptor(\User.joinDate),
+                            ])
+
+                        Text("Sort by Join Date")
+                            .tag([
+                                SortDescriptor(\User.joinDate),
+                                SortDescriptor(\User.name)
+                            ])
+                    }
+                }
+                Button(showingUpcomingOnly ? "Show Everyone" : "Show Upcoming") {
+                    showingUpcomingOnly.toggle()
+                }
+                
+                Button("Add user", systemImage: "plus") {
+                    let user = User(name: "", city: "", joinDate: .now)
+                    modelContext.insert(user)
+                    path = [user]
+                }
+                Button("Add Samples", systemImage: "plus.circle") {
+                    try? modelContext.delete(model: User.self)
+                    
+                           let first = User(name: "Ed Sheeran", city: "London", joinDate: .now.addingTimeInterval(86400 * -10))
+                           let second = User(name: "Rosa Diaz", city: "New York", joinDate: .now.addingTimeInterval(86400 * -5))
+                           let third = User(name: "Roy Kent", city: "London", joinDate: .now.addingTimeInterval(86400 * 5))
+                           let fourth = User(name: "Johnny English", city: "London", joinDate: .now.addingTimeInterval(86400 * 10))
+
+                           modelContext.insert(first)
+                           modelContext.insert(second)
+                           modelContext.insert(third)
+                           modelContext.insert(fourth)
+                       }
+            }
         }
-        .padding()
     }
 }
 
